3dafb2cf83d05ddf349c9f07c54350f5e1fa1378
hmasood21955 <hmasood878@gmail.com>
2023-12-18 15:20:36 +0500

    toGraysacleLightness

--- colorUtils.c
+++ colorUtils.c
@@ -15,3 +15,7 @@ int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
 }
+int toGraysacleLightness(int r,int g, int b)
+{
+  int max = r;
+  int min = r;
